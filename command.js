2563df35eb2c44ce4c5f1a95a4baf61d511e2e8d
peterdemartini <thepeterdemartini@gmail.com>
2017-07-24 13:34:35 -0700

    it is alive!

---
+++ command.js
@@ -0,0 +1,48 @@
+const bindAll = require("lodash/fp/bindAll")
+const OctoDash = require("octodash")
+const Server = require("./lib/server")
+const packageJSON = require("./package.json")
+
+const CLI_OPTIONS = [
+  {
+    names: ["client-access-token"],
+    type: "string",
+    required: true,
+    env: "CLIENT_ACCESS_TOKEN",
+    help: "API.AI client access token",
+    helpArg: "API_AI_CLIENT_ACCESS_TOKEN",
+  },
+  {
+    names: ["port", "p"],
+    type: "number",
+    env: "PORT",
+    default: 80,
+    required: true,
+    help: "Port to run server as",
+    helpArg: "NUMBER",
+  },
+]
+
+class Command {
+  constructor() {
+    bindAll(Object.getOwnPropertyNames(Command.prototype), this)
+    this.octoDash = new OctoDash({
+      argv: process.argv,
+      cliOptions: CLI_OPTIONS,
+      name: packageJSON.name,
+      version: packageJSON.version,
+    })
+  }
+
+  run() {
+    const { clientAccessToken, port } = this.octoDash.parseOptions()
+    new Server({ clientAccessToken, port }).run(error => {
+      if (error) {
+        this.octoDash.die(error)
+        return
+      }
+      console.log(`Running ${packageJSON.name} on port ${port}`)
+    })
+  }
+}
+new Command().run()
